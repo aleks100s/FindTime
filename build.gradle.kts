df9d2fb5474887295a4b6f63a9b734f564f3b238
aleks100s <aleksandr100s@yandex.ru>
2023-05-09 12:10:01 +0300

    Update Android app and lib plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ buildscript {
         classpath(Deps.kotlin_gradle_plugin)
     }
 }
+
 plugins {
-    //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("7.4.1").apply(false)
-    id("com.android.library").version("7.4.1").apply(false)
+    id(androidApp).version(Versions.android_gradle_plugin).apply(false)
+    id(androidLib).version(Versions.android_gradle_plugin).apply(false)
     kotlin(androidPlugin).version(Versions.kotlin).apply(false)
     kotlin(multiplatform).version(Versions.kotlin).apply(false)
 }
